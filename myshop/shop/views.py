05f70009202ca2feb365d27d710075e9b315bbbc
Adam Parr <adamparr.uk@gmail.com>
2024-12-08 15:10:26 +0000

    Add carts app and functionality, including add/remove/view cart, update cart in detail view

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
 from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 def product_list(request, category_slug=None):
     category = None
@@ -24,8 +23,9 @@ def product_detail(request, id, slug):
     product = get_object_or_404(
         Product, id=id, slug=slug, available=True
     )
+    cart_product_form = CartAddProductForm()
     return render(
         request,
         'shop/product/detail.html',
-        { 'product': product }
+        { 'product': product, 'cart_product_form': cart_product_form }
     )
