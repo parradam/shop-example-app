66d8c4839cf4b5780fa0602a7d809050dcd1593b
Adam Parr <adamparr.uk@gmail.com>
2024-12-10 20:04:33 +0000

    Add custom export to CSV option to admin

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
+import csv
+import datetime
+from django.http import HttpResponse
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = (
+        f'attachment; filename={opts.verbose_name}.csv'
+    )
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    # Write headers
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
 
 def order_payment(obj):
     url = obj.get_stripe_url()
@@ -31,3 +61,4 @@ class OrderAdmin(admin.ModelAdmin):
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
